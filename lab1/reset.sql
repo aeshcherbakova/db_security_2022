6474e5e9009cc9a44875babe363490aee759f80f
alex <aeshcherbakova@yandex.ru>
2022-05-31 14:55:29 +0300

    lab1 reset added

---
+++ lab1/reset.sql
@@ -0,0 +1,8 @@
+drop table goods cascade constraints;
+drop table purchase cascade constraints;
+drop table purchase_item cascade constraints;
+drop table sales cascade constraints;
+drop table customers cascade constraints;
+drop table categories cascade constraints;
+drop table suppliers cascade constraints;
+@lab1.sql
